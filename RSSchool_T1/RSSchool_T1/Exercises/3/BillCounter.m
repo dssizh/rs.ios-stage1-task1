8a0973bbe7f5d2f28daa16cbbbb4dba3e3107612
Dmitriy S <sutyginds@gmail.com>
2020-03-17 00:37:48 +0400

    feat: task3 done

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,21 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    
+    int total = 0;
+    for (int i=0; i < bill.count; i++) {
+        if (i == index)
+            continue;
+        total = total + [[bill objectAtIndex:i] intValue];
+    }
+    
+    int result = [sum intValue] - total/2;
+    
+    if (result == 0) {
+        return @"Bon Appetit";
+    } else {
+        return [[NSNumber numberWithInt:result] stringValue];
+    }
 }
 
 @end
