1a8aa5756a191ccfe0600f5039a312d972b1afd8
Dmitriy S <sutyginds@gmail.com>
2020-03-16 22:16:44 +0400

    feat: task1 done

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,36 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    
+    if (sadArray.count == 0)
+        return sadArray;
+    
+    Boolean check = true;
+    NSMutableArray *happyArray = [[NSMutableArray alloc] init];
+    while (check) {
+        
+        check = false;
+        
+        [happyArray addObject:[sadArray objectAtIndex: 0]];
+        for (int i=1; i<sadArray.count-1; i++) {
+            
+            int a = [[sadArray objectAtIndex: i-1] intValue];
+            int b = [[sadArray objectAtIndex: i+1] intValue];
+            if ((a + b) < [[sadArray objectAtIndex: i] intValue]){
+                check = true;
+            } else {
+                [happyArray addObject:[sadArray objectAtIndex: i]];
+            }
+        }
+        
+        [happyArray addObject:[sadArray objectAtIndex: sadArray.count-1]];
+        if (check) {
+            sadArray = happyArray;
+            happyArray = [[NSMutableArray alloc] init];
+        }
+    }
+    
+    return happyArray;
 }
 
 @end
